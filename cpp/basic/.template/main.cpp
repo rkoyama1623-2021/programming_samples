5f27d0705f7f2801bbf6f4da06805ecd907020c0
Ryo KOYAMA <koyama+git@jsk.imi.i.u-tokyo.ac.jp>
2016-05-20 18:17:39 +0900

    [cpp/basic/.template/] add sample for c++ (#4)

---
+++ cpp/basic/.template/main.cpp
@@ -0,0 +1,7 @@
+// -*- coding : utf-8 -*-
+// -*- c-basic-offset : 2 -*-
+#include <iostream>
+int main () {
+  std::cout << "hello world!" << std::endl;
+  return 0;
+};
---
+++ test.sh
@@ -0,0 +1,33 @@
+function usage (){
+    echo "this is command for test"
+}
+function execute (){
+    echo "--->" $@
+    if ! ${ECHO}; then ${ECHO} $@; fi;
+}
+OPT=`getopt -o hipeynsr: -l help, -- $*`
+eval set -- $OPT
+while [ -n "$1" ] ; do
+    case $1 in
+	-h|--help) usage;shift ;;
+        -n) shift;ECHO="echo";;
+	--) shift; break;;
+	*) echo "Unknown option($1)"; usage;shift;;
+    esac
+done
+
+testList=(cpp/basic/.template/build.sh cpp/basic/inheritance/build.sh)
+topDir=$(pwd)
+for test in ${testList[@]}; do
+    execute cd ${topDir}
+    execute cd $(dirname ${test})
+    execute ./$(basename ${test})
+    if ! test ${?} -eq 0;then 
+        echo "test fail.";
+    else
+        echo "test pass!"
+    fi
+    if [ -e build ];then execute rm -rf build; fi;
+done
+execute cd ${topDir}
+
