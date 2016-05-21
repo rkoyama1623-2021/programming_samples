548296087965b91d6707d28c12688a941aa13aff
KOYAMA Ryo <koyama@jsk.imi.i.u-tokyo.ac.jp>
2016-05-21 21:07:50 +0900

    [cpp/hrpsys/sequence_player/build.sh] update

--- cpp/hrpsys/sequence_player/build.sh
+++ cpp/hrpsys/sequence_player/build.sh
@@ -2,8 +2,9 @@ if [ -e build ];then rm -rf build;fi
 mkdir build;cd build;
 cmake ..
 make
+if ! [ -e $? ];then export EXIT_STATUS=$?;fi
 cd ..
 
 ./build/main
+if ! [ -e $? ];then export EXIT_STATUS=$?;fi
 gnuplot -p plot.plt
-
