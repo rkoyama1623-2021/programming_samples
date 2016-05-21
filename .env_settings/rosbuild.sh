f5e8ac94c4c77f1904677b3fc6a6ff0b4e4dcbd6
KOYAMA Ryo <koyama@jsk.imi.i.u-tokyo.ac.jp>
2016-05-21 22:40:27 +0900

    update

--- .env_settings/rosbuild.sh
+++ .env_settings/rosbuild.sh
@@ -4,8 +4,24 @@ mkdir -p ${HOME}/ros/indigo_parent/src
 cp ${REPOSITORY_TOP}/.env_settings/.rosinstall ${HOME}/ros/indigo_parent/src/
 cd ${HOME}/ros/indigo_parent/src
 wstool update
+cd ..
+rosdep install --from-paths src --ignore-src -r
 cd ${HOME}/ros/indigo_parent
 catkin build -ci
 source $HOME/ros/indigo_parent/devel/setup.bash
 
+cat "function catkin () {
+/usr/bin/catkin ${@};
+cp ${REPOSITORY_TOP}/.env_settings/.catkin /home/koyama/ros/indigo_parent/devel/.catkin;
+}" >> ${HOME}/.bashrc
+source ${HOME}/.bashrc
+sudo updatedb
 cd ${REPOSITORY_TOP}
+
+echo "-------------------"
+echo "current directory"
+echo "$(pwd)"
+echo "-------------------"
+
+
+
