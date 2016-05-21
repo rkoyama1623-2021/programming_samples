2a25b140c6fd4c0eb20353cb36b17fdb2255ba63
KOYAMA Ryo <koyama@jsk.imi.i.u-tokyo.ac.jp>
2016-05-21 22:15:00 +0900

    [.env_settings/rosbuild.sh] update

--- .env_settings/rosbuild.sh
+++ .env_settings/rosbuild.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 ## indigo_parent
 mkdir -p ${HOME}/ros/indigo_parent/src
+cp ${REPOSITORY_TOP}/.env_settings/.rosinstall ${HOME}/ros/indigo_parent/src/
 cd ${HOME}/ros/indigo_parent/src
-cp ${REPOSITORY_TOP}/.env_settings/.rosinstall
 wstool update
 cd ${HOME}/ros/indigo_parent
 catkin build -ci
