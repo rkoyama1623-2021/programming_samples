4aa32a0a243a16ec8191b1367877bdee2e59e94a
KOYAMA Ryo <koyama@jsk.imi.i.u-tokyo.ac.jp>
2016-05-23 16:06:29 +0900

    [cpp/basic/.template] update

--- cpp/basic/.template/build.sh
+++ cpp/basic/.template/build.sh
@@ -1,7 +1,11 @@
+#!/usr/bin/env bash
+
 if [ -e build ];then rm -rf build;fi
 mkdir build;cd build;
 cmake ..
-make
+make; if ! $(exit $?);then EXIT_STATUS=1;fi
 cd ..
 
-./build/main
+./build/main; if ! $(exit $?);then EXIT_STATUS=1;fi
+
+exit $EXIT_STATUS
