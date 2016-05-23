168aba1656e063e2f0c3d1103a5df44a62512809
KOYAMA Ryo <koyama@jsk.imi.i.u-tokyo.ac.jp>
2016-05-23 16:24:36 +0900

    [test.sh] update

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ function usage (){
 }
 function execute (){
     echo "--->" $@
-    if ! ${ECHO}; then ${ECHO} $@; fi;
+    if ! ${ECHO}; then ${ECHO} $@; else ${@}; fi;
 }
 OPT=`getopt -o hipeynsr: -l help, -- $*`
 eval set -- $OPT
@@ -16,18 +16,25 @@ while [ -n "$1" ] ; do
     esac
 done
 
-testList=(cpp/basic/.template/build.sh cpp/basic/inheritance/build.sh)
+testList=(
+    cpp/basic/.template/build.sh
+    cpp/basic/inheritance/build.sh
+    cmake/03_find_package/build.sh
+)
 topDir=$(pwd)
+
+export EXIT_STATUS=0;
 for test in ${testList[@]}; do
     execute cd ${topDir}
     execute cd $(dirname ${test})
     execute ./$(basename ${test})
     if ! test ${?} -eq 0;then 
         echo "test fail.";
+        export EXIT_STATUS=1;
     else
         echo "test pass!"
     fi
     if [ -e build ];then execute rm -rf build; fi;
 done
 execute cd ${topDir}
-
+exit ${EXIT_STATUS}
