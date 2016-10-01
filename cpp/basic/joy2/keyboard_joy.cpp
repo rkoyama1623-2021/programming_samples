aa635b5dab312bf44e6f46901fd019882b0f8861
KOYAMA Ryo <koyama@jsk.imi.i.u-tokyo.ac.jp>
2016-10-01 18:54:09 +0900

    [cpp/basic/joy2/keyboard_joy.cpp] use std::vector

--- cpp/basic/joy2/keyboard_joy.cpp
+++ cpp/basic/joy2/keyboard_joy.cpp
@@ -19,20 +19,20 @@ public:
   void read();
 private:
   char* key;
-  const std::string KEYCODE_U;
-  const std::string KEYCODE_D;
-  const std::string KEYCODE_R;
-  const std::string KEYCODE_L;
+  const std::vector<char> KEYCODE_U;
+  const std::vector<char> KEYCODE_D;
+  const std::vector<char> KEYCODE_R;
+  const std::vector<char> KEYCODE_L;
 };
 // keyboard_joy::keyboard_joy() : GetCharacter() {
 //   keyboard_joy(3,0);
 // };
 keyboard_joy::keyboard_joy(int tm_sec, int tm_usec)
   :GetCharacter(tm_sec, tm_usec),
-   KEYCODE_U((const char[]) {0x1b, 0x5b, 0x41, 0x00}),
-   KEYCODE_D((const char[]) {0x1b, 0x5b, 0x42, 0x00}),
-   KEYCODE_R((const char[]) {0x1b, 0x5b, 0x43, 0x00}),
-   KEYCODE_L((const char[]) {0x1b, 0x5b, 0x44, 0x00})
+   KEYCODE_U{0x1b, 0x5b, 0x41},
+   KEYCODE_D{0x1b, 0x5b, 0x42},
+   KEYCODE_R{0x1b, 0x5b, 0x43},
+   KEYCODE_L{0x1b, 0x5b, 0x44}
 {
   key = NULL;
 };
@@ -41,16 +41,21 @@ keyboard_joy::~keyboard_joy() {
 };
 void keyboard_joy::read() {
   GetCharacter::read(&key, 3);
-  char tmp_key[3]={0};
-  //strcpy(tmp_key,key);
-  memcpy(tmp_key,key,3);
-  const std::string key_str(tmp_key, tmp_key+3);
-  if ( key_str == KEYCODE_U) {
+  const std::vector<char> key_vec(key, key+3);
+  if ( key_vec == KEYCODE_U) {
     printf("UP!\n");
+  } else if ( key_vec == KEYCODE_D) {
+    printf("DOWN!\n");
+  } else if ( key_vec == KEYCODE_R) {
+    printf("RIGHT!\n");
+  } else if ( key_vec == KEYCODE_L) {
+    printf("LEFT!\n");
+  } else {
+    printf("key_vec: %d/%d/%d (%d)\n", key_vec[0], key_vec[1], key_vec[2], (int) key_vec.size());
   }
-  printf("key_str: %d/%d/%d (%d)\n", key_str[0], key_str[1], key_str[2], key_str.length());
-  printf("key:     %d/%d/%d\n", key[0], key[1], key[2]);
-  printf("UP:      %d/%d/%d (%d)\n", KEYCODE_U[0], KEYCODE_U[1], KEYCODE_U[2], KEYCODE_U.length());
+  // printf("key_vec: %d/%d/%d (%d)\n", key_vec[0], key_vec[1], key_vec[2], key_vec.size());
+  // printf("key:     %d/%d/%d\n", key[0], key[1], key[2]);
+  // printf("UP:      %d/%d/%d (%d)\n", KEYCODE_U[0], KEYCODE_U[1], KEYCODE_U[2], KEYCODE_U.size());
 };
 
 int main(void)
