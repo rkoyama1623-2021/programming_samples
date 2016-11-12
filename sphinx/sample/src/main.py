22e5a54f03590a455fa0eb5b925f584dc6f7d822
KOYAMA Ryo <koyama@jsk.imi.i.u-tokyo.ac.jp>
2016-11-13 05:47:43 +0900

    [sphinx/sample/src/main.py] update

--- sphinx/sample/src/main.py
+++ sphinx/sample/src/main.py
@@ -3,9 +3,20 @@ import sys
 
 def main(name, age=None):
     """Greeting function.
+    check `here for detail <http://thomas-cokelaer.info/tutorials/sphinx/docstring_python.html>`_
+
+    .. function:: main.main(name, age = None)
 
     :param name: Your name.
     :param age: Youre age. (option)
+
+    :param arg1: description
+    :param arg2: description
+    :type arg1: type description
+    :type arg1: type description
+    :return: return description
+    :rtype: the return type description
+
     """
     print "hello, " + name
 
